55659bb0c5a3e830db2856e28eccb57efa9c6628
danielmccoy <danielmccoy@gmail.com>
2012-07-04 17:49:01 +0000

    Initial template folder relocation

--- trunk/framework/config/Path.php
+++ trunk/framework/config/Path.php
@@ -3,9 +3,9 @@
 	class Path_Config
 	{
 		// Remember trailing slash on all paths
-		public $web_path		= "/project-framework/";
-		public $full_web_path		= "http://localhost/project-framework/";
-		public $application_path	= "/home/test/www/project-framework/";
+		public $web_path		= "/";
+		public $full_web_path		= "http://192.168.1.104/";
+		public $application_path	= "/var/www/";
 		public $template_name		= "flicker";
 		public $log_in_controller	= "dashboard";
 	}
