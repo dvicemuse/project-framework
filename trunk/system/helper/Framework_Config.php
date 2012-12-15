ffa00ec6d74c361ce3aeb200df69180d4f7b42b3
danielmccoy@gmail.com <danielmccoy@gmail.com@4a762454-122f-5177-6b00-4384a381ff7a>
2012-12-15 04:46:29 +0000

    Do not expose config vars... less efficient but more secure

--- trunk/system/helper/Framework_Config.php
+++ trunk/system/helper/Framework_Config.php
@@ -18,14 +18,17 @@
 			}
 			return self::$_instance;
 		}
-		
-		
+
+
+
 		/**
 		 * Auto load the config info
 		 * @return Framework_Config
 		 */
 		public function load()
 		{
+			$config = new Config_Builder;
+			
 			// Include configuration classes
 			$handle = opendir(substr(__DIR__, 0, -14)."/framework/config/");
 			while(false !== ($file = readdir($handle)))
@@ -39,18 +42,18 @@
 					$class_vars = get_class_vars($config_class_name);
 					if(is_array($class_vars))
 					{
-						if(empty($this->$config_class_base_name))
+						if(empty($config->$config_class_base_name))
 						{
-							$this->$config_class_base_name = new Config_Builder;
+							$config->$config_class_base_name = new Config_Builder;
 						}
 						foreach($class_vars as $name => $value)
 						{
-							$this->$config_class_base_name->$name = $value;
+							$config->$config_class_base_name->$name = $value;
 						}
 					}
 				}
 			}
-			return $this;
+			return $config;
 		}
 		
 		
