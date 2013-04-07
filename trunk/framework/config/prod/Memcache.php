e89b4482085ad1fa6fe0c791b872bc22d62317d0
danielmccoy <danielmccoy@gmail.com>
2013-04-07 22:29:42 +0000

    Memcache initial config (this will be interesting)

---
+++ trunk/framework/config/prod/Memcache.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @file Memcache.php
+ * @package    ProjectFramework.Config
+ *
+ * @license    see LICENSE.txt
+ */
+
+/**
+ * @class Memcache_Config
+ * @brief Memcache Config class.
+ *
+ * @since    1.0.0
+ */
+class Memcache_Config
+{
+	/**
+	 * @var string $host
+	 * @brief The host address to connect to memcache
+	 *
+	 * @since  1.0.0
+	 */
+	public $host	= '';
+	
+	/**
+	 * @var string $port	
+	 * @brief The port to connect to memcache
+	 *
+	 * @since  1.0.0
+	 */
+	public $port	= '';
+
+	
+}
